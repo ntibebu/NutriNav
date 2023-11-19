83ca875d17f29a0241f49ac462aad2fb83a695db
ntibebu <123273236+ntibebu@users.noreply.github.com>
2023-11-18 20:46:08 -0500

    Add files via upload

---
+++ python_example.py
@@ -0,0 +1,10 @@
+import openai
+openai.api_key="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"
+completion = openai.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+print(completion.choices[0].message.content)
